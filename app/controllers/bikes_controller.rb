2c8b42e0ec029ddc40ee68604acc2111a93e77d5
Sau Wai Chan <sauwaic@gmail.com>
2016-11-22 11:03:52 +0100

    created bike controller

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,3 +1,47 @@
 class BikesController < ApplicationController
-end
 
+  before_action :set_bike, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @bikes = Bike.all
+  end
+
+  def show
+  end
+
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+    bike = Bike.new(bike_parameters)
+    bike.user = current_user
+    bike.save
+    redirect_to bikes_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    @bike.update(bike_parameters)
+    redirect_to bike_path(@bike)
+  end
+
+  def destroy
+    @bike.destroy
+    redirect_to bikes_path
+  end
+
+  private
+
+  def set_bike
+    @bike = Bike.find(params[:id])
+  end
+
+  def bike_parameters
+    params.require(:bike).permit(:size, :gender, :category, :gears, :picture_url, :condition, :price)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# TODO: Please first create two Users via browser
+
+# Creating list of accessories
+Accessory.destroy_all
+
+%w(front-light back-light basket trolley back-seat lock).each do |accessory|
+  Accessory.create(name: accessory)
+end
+
+#Creating bikes
+Bike.destroy_all
+
+bike1_attributes = {
+  size: "small",
+  gender: "female",
+  category: "racing",
+  gears: 3,
+  picture_url: "www.lalala.com",
+  condition: "good",
+  price: 100
+}
+
+bike2_attributes = {
+  size: "large",
+  gender: "male",
+  category: "racing",
+  gears: 3,
+  picture_url: "www.pqoweiw.com",
+  condition: "good",
+  price: 200
+}
+
+User.first.bikes.build(bike1_attributes).save
+User.last.bikes.build(bike2_attributes).save
+
+#Associating accessories with bikes
+BikeAccessory.create(bike_id: 1, accessory_id: 25)
+BikeAccessory.create(bike_id: 1, accessory_id: 26)
