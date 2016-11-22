13d1c8e36f7965ca7b9187cb99f106f9ec046a54
Sau Wai Chan <sauwaic@gmail.com>
2016-11-22 13:37:47 +0100

    placeholders for User pages inserted

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,9 +4,11 @@ class DashboardsController < ApplicationController
   end
 
   def my_bookings
+    @bookings = current_user.bookings
   end
 
   def my_bikes
+    @bikes = current_user.bikes
   end
 
 end
