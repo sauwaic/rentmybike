38b6ec2bd39d024abf29f84b0b1441ed93dff770
thibault de combarieu <tdecomba@gmail.com>
2016-11-23 16:54:46 +0100

    added address to bikes & lat/lng to bikes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122134958) do
+ActiveRecord::Schema.define(version: 20161123155311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 20161122134958) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_bikes_on_user_id", using: :btree
   end
 
